c9c55d63dfb5361a5b6d128705c6a80a3e2fa124
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-08-04 12:06:44 +0800

    Creating a Table

--- Chapter1.sql
+++ Chapter1.sql
@@ -1,3 +1,14 @@
 --Creating a Database analysis
 
 CREATE DATABASE analysis;
+
+--Creating a Table
+
+CREATE TABLE teachers (
+    id bigserial,
+    first_name varchar(25),
+    last_name varchar(50),
+    school varchar(50),
+    hire_date date,
+    salary numeric
+);
