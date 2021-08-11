a96481ce641786a32fe3aad8a77ed01443f854a6
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-08-11 08:09:24 +0800

    numeric,float, double

--- Chapter3.sql
+++ Chapter3.sql
@@ -5,3 +5,24 @@ CREATE TABLE char_data_types (
     char_column char(10),
     text_column text
 )
+
+CREATE TABLE number_data_types(
+    numeric_column numeric(20,5),
+    real_column real,
+    double_column double precision
+);
+
+INSERT INTO number_data_types
+VALUES
+(.7, .7, .7),
+(2.13579, 2.13579, 2.13579),
+(2.1357987654, 2.1357987654, 2.1357987654);
+
+SELECT * FROM number_data_types;
+
+-- Float is not exact. If we want exact precisions, use numeric(precision,scale)
+SELECT 
+numeric_column * 10000000 AS "Fixed",
+real_column * 10000000 AS "Float"
+FROM number_data_types
+WHERE numeric_column = 0.7
