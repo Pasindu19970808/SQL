7ed7d29056ff091dad8606dde1f02a2a9d71208e
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-08-12 11:15:10 +0800

    date_time chapter

--- Chapter3.sql
+++ Chapter3.sql
@@ -26,3 +26,17 @@ numeric_column * 10000000 AS "Fixed",
 real_column * 10000000 AS "Float"
 FROM number_data_types
 WHERE numeric_column = 0.7
+
+-- Dates and Times
+
+CREATE TABLE data_time_types(
+    timestamp_column timestamp with time zone,
+    interval_column interval 
+);
+
+INSERT INTO data_time_types
+VALUES
+('2018-12-31 01:00 EST','2 days'),
+('2018-12-31 01:00 -8','1 month'),
+('2018-12-31 01:00 Australia/Melbourne','1 century'),
+(now(),'1 week')
