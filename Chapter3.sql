b4ebde683711aa9c56735d683ff47bd347794495
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-08-13 08:25:57 +0800

    timestamps and intervals

--- Chapter3.sql
+++ Chapter3.sql
@@ -39,4 +39,9 @@ VALUES
 ('2018-12-31 01:00 EST','2 days'),
 ('2018-12-31 01:00 -8','1 month'),
 ('2018-12-31 01:00 Australia/Melbourne','1 century'),
-(now(),'1 week')
+(now(),'1 week');
+
+SELECT timestap_column,
+        interval_column,
+        timestamp_column - interval_column as new_date
+FROM data_time_types;
