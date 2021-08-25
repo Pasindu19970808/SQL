698c5500f2aadbdda1ae6a9fdc1210542b584297
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-08-25 08:46:05 +0800

    Aggregate functions, medians

--- Chapter5.sql
+++ Chapter5.sql
@@ -1,8 +1,9 @@
 --Basic Math and Stats with SQL
 
+-- Concatenation syntax
+
 SELECT CONCAT(CAST((SELECT 11 / 6) AS text),'.',CAST((SELECT 11 % 6) AS text));
 
--- Concatenation syntax
 SELECT employee_id,first_name,last_name,
 concat(first_name,' ',last_name) "Name of the Employee" 
 FROM employees
@@ -37,3 +38,59 @@ SELECT geo_name,state_us_abbreviation,
 FROM us_counties_2010
 
 SELECT * FROM Pct_Asian
+
+--Tracking Percentage Change
+CREATE TABLE percent_change (
+ department varchar(20),
+ spend_2014 numeric(10,2),
+ spend_2017 numeric(10,2)
+);
+
+INSERT INTO percent_change
+VALUES
+('Building', 250000, 289000),
+('Assessor', 178556, 179500),
+('Library', 87777, 90001),
+('Clerk', 451980, 650000),
+('Police', 250000, 223000),
+('Recreation', 199000, 195000);
+
+SELECT * FROM percent_change;
+
+SELECT department, 
+round((((spend_2017 - spend_2014)/spend_2014)*100),2) as "pct_change"
+FROM percent_change
+
+SELECT sum(p0010001) AS "County Sum",
+round(avg(p0010001), 0) AS "County Average"
+FROM us_counties_2010;
+
+SELECT * 
+FROM percent_change
+ORDER BY spend_2014;
+
+SELECT
+(percentile_cont(0.5)
+WITHIN GROUP(ORDER BY spend_2014)),
+percentile_disc(0.5)
+WITHIN GROUP(ORDER BY spend_2014),
+percentile_cont(0.5)
+WITHIN GROUP(ORDER BY spend_2017),
+percentile_disc(0.5)
+WITHIN GROUP(ORDER BY spend_2017)
+FROM percent_change
+
+SELECT sum(p0010001) AS "County Sum",
+round(avg(p0010001), 0) AS "County Average",
+percentile_cont(0.5)WITHIN GROUP(ORDER BY p0010001) AS "Country Median"
+FROM us_counties_2010;
+
+SELECT 
+percentile_cont(array [0.25,0.5,0.75])
+WITHIN GROUP(ORDER BY p0010001) As "quartiles"
+FROM us_counties_2010;
+
+--creating JSON arrays with key:value pairs
+SELECT 
+json_object_agg(state_us_abbreviation,p0010001)
+FROM us_counties_2010;
